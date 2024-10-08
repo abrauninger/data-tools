0b2ffccd88f568b175bc01efc2eacb2671959fd9
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2024-10-07 17:44:37 -0700

    Execute 'pdftotext' and 'tr' programmatically

--- extractors/p223_pdf_batch.py
+++ extractors/p223_pdf_batch.py
@@ -2,6 +2,9 @@ import glob
 import shutil
 import subprocess
 import sys
+import tempfile
+
+import p223_pdf_to_csv
 
 pdftotext = shutil.which('pdftotext')
 if pdftotext is None:
@@ -15,12 +18,24 @@ pdf_paths = glob.glob(f'../input/*.pdf')
 
 pdf_path = pdf_paths[0]
 
-breakpoint()
+with tempfile.NamedTemporaryFile(delete_on_close=False) as squished_tempfile:
+	# '-f 2' to start processing on the second page of the PDF (since page 1 is a cover page).
+	pdftotext_process = subprocess.Popen([pdftotext, '-layout', pdf_path, '-f', '2', '-'], stdout=subprocess.PIPE)
+	tr_process = subprocess.Popen([tr, '-s', "' '"], stdin=pdftotext_process.stdout, stdout=squished_tempfile)
+	pdftotext_process.stdout.close()
+	out, err = tr_process.communicate()
 
-# '-f 2' to start processing on the second page of the PDF (since page 1 is a cover page).
-pdftotext_process = subprocess.Popen([pdftotext, '-layout', pdf_path, '-f', '2', '-'], stdout=subprocess.PIPE)
-tr_process = subprocess.Popen([tr, '-s', "' '"], stdin=pdftotext_process.stdout, stdout=subprocess.PIPE)
-pdftotext_process.stdout.close()
-out, err = tr_process.communicate()
+	if err is not None:
+		sys.exit(f"'pdftotext' and/or 'tr' had an error:\n{err}")
+
+	if out is not None:
+		print(f"'pdftotext' and/or 'tr' produced output on stdout, which was unexpected:\n{out}")
+		
+	breakpoint()
+
+	squished_tempfile.close()
+
+	output_file = '../output/out.csv'
+	p223_pdf_to_csv.main(squished_tempfile.name, output_file)
 
 print("All done")
