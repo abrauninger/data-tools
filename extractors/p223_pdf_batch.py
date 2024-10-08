be00a954e9065d36e3b8b96ef1460ce4bb15170f
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2024-10-07 18:06:16 -0700

    Write CSV files for each monthly PDF

--- extractors/p223_pdf_batch.py
+++ extractors/p223_pdf_batch.py
@@ -5,12 +5,33 @@ $ python3 p223_batch.py
 """
 
 import glob
+import os
+import re
+import pathlib
 import shutil
 import subprocess
 import sys
 import tempfile
+import time
 
 import p223_pdf_to_csv
+
+
+def month_from_pdf_file_name(pdf_path) -> str:
+    filename = os.path.basename(pdf_path)
+
+    m = re.match(r'^P223_(\D+)(\d+)\.pdf$', filename)
+    if m is None:
+        raise Exception(f"Unable to determine month and year from filename: '{filename}'")
+
+    month_name_abbreviated = m.group(1)
+    year_two_digit = m.group(2)
+
+    parsed_month = time.strptime(f'{month_name_abbreviated} {year_two_digit}', '%b %y')
+    month = time.strftime('%Y-%m', parsed_month)
+
+    return month
+
 
 def main(input_directory, output_directory):
     pdftotext = shutil.which('pdftotext')
@@ -23,25 +44,29 @@ def main(input_directory, output_directory):
 
     pdf_paths = glob.glob(f'{input_directory}/*.pdf')
 
-    pdf_path = pdf_paths[0]
+    for pdf_path in pdf_paths:
+        with tempfile.NamedTemporaryFile(delete_on_close=False) as squished_tempfile:
+            # '-f 2' to start processing on the second page of the PDF (since page 1 is a cover page).
+            pdftotext_process = subprocess.Popen([pdftotext, '-layout', pdf_path, '-f', '2', '-'], stdout=subprocess.PIPE)
+            tr_process = subprocess.Popen([tr, '-s', "' '"], stdin=pdftotext_process.stdout, stdout=squished_tempfile)
+            pdftotext_process.stdout.close()
+            out, err = tr_process.communicate()
 
-    with tempfile.NamedTemporaryFile(delete_on_close=False) as squished_tempfile:
-        # '-f 2' to start processing on the second page of the PDF (since page 1 is a cover page).
-        pdftotext_process = subprocess.Popen([pdftotext, '-layout', pdf_path, '-f', '2', '-'], stdout=subprocess.PIPE)
-        tr_process = subprocess.Popen([tr, '-s', "' '"], stdin=pdftotext_process.stdout, stdout=squished_tempfile)
-        pdftotext_process.stdout.close()
-        out, err = tr_process.communicate()
+            if err is not None:
+                sys.exit(f"'pdftotext' and/or 'tr' had an error:\n{err}")
 
-        if err is not None:
-            sys.exit(f"'pdftotext' and/or 'tr' had an error:\n{err}")
+            if out is not None:
+                print(f"'pdftotext' and/or 'tr' produced output on stdout, which was unexpected:\n{out}")
 
-        if out is not None:
-            print(f"'pdftotext' and/or 'tr' produced output on stdout, which was unexpected:\n{out}")
+            squished_tempfile.close()
 
-        squished_tempfile.close()
+            month = month_from_pdf_file_name(pdf_path)
+            output_file = f'{output_directory}/month/{month}.csv'
+            pathlib.Path(os.path.dirname(output_file)).mkdir(parents=True, exist_ok=True)
 
-        output_file = f'{output_directory}/out.csv'
-        p223_pdf_to_csv.main(squished_tempfile.name, output_file)
+            p223_pdf_to_csv.main(squished_tempfile.name, output_file)
+
+            print(f"Extracted {pdf_path} to {output_file}")
 
 
 if __name__ == '__main__':
